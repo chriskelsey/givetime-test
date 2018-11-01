f346cc98a9a1487364881de7403a2af4a12b8f4f
chriskelsey <chriskelsey.net@gmail.com>
2018-11-01 10:57:04 -0700

    Event feed updates

--- client/src/components/Feed/FeedListItem.js
+++ client/src/components/Feed/FeedListItem.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./Feed.css";
 import "../Button/Button-2.css";
 import API from "../../utils/API";
-
-API.getEvents().then(event =>{console.log(event.data);});
-   
+  
 
 const FeedListItem = props => (
-    <div className = "callout clearfix horizontal-center">
-        <h5>
-            Example Opportunity
-            <a href='#' className="button radius float-right eventBtn">See Event</a>
-        </h5>
+	//API.getEvents().then(event =>{
+		//event.data.forEach( (data) =>{
+			<div className = "callout clearfix horizontal-center">
+        	<h5>
+        		Event Name
+            	<a href='#' className="button radius float-right eventBtn">See Event</a>
+        	</h5>
 
         <p className="feed">
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
         </p>
     </div>
+
+		//});
+	//})
+    
 );
 
 export default FeedListItem;
