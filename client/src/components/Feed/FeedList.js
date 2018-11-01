6c1ab3d017420e5bc227d3241546357d29af247f
chriskelsey <chriskelsey.net@gmail.com>
2018-10-31 19:43:11 -0700

    reorganizing client files

--- client/src/components/Feed/FeedList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Feed.css";
-
-const FeedList = props => (
-  <ul className="feedList absolute-center">{props.children}</ul>
-);
-
-export default FeedList;
--- client/src/components/SignInPage/CharityEventPage/CharityFeedList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./CharityEvent.css";
-
-const CharityFeedList = props => (
-  <ul className="feedList absolute-center">{props.children}</ul>
-);
-
-export default CharityFeedList;
