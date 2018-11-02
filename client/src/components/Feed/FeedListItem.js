73c45729d3f716347351c539a82ded01768e11ca
Tam Hong <tamhong.sd@gmail.com>
2018-11-01 18:26:07 -0700

    routing fix

--- client/src/components/Feed/FeedListItem.js
+++ client/src/components/Feed/FeedListItem.js
@@ -7,11 +7,7 @@ const FeedListItem = props => (
     <div className = "callout clearfix horizontal-center">
         <h5>
             {props.name}
-<<<<<<< HEAD
             <Link to ="/eventsignup"className="button radius float-right ">Sign Up</Link>
-=======
-            <a href="/eventsignup" className="button radius float-right eventBtn">Sign Up</a>
->>>>>>> 7018659ce6b64db58d779081753a78587c7db88c
         </h5>
         <h6>{props.date}</h6>
         <p className="feed">
